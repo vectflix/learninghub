aa2ac359df8d7d940a690404b0cdb8e9f503f6f1
vectflix <malcolmotieno69@gmail.com>
2026-02-07 23:30:22 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,4 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Supabase env vars missing!');
+}
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
